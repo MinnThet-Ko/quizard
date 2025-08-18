545d8700c2723aeea8756a049a2d4a8e09d0edc0
MinnThet-Ko <blltspd@gmail.com>
2025-08-18 18:15:50 +0900

    Fixed the env

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGIN_SENDER_ID,
-    appId: process.env.APP_ID
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
 
+console.log(import.meta.env.FIREBASE_API_KEY)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Define 'process' as an object with 'env' property
-    'process': {
-      'env': {
-        FIREBASE_API_KEY:"AIzaSyB0FyRYmPYVPdYh7IEs0VrZdbh1Zc2nU-M",
-        FIREBASE_AUTH_DOMAIN:"flashcard-abf29.firebaseapp.com",
-        PROJECT_ID:"flashcard-abf29",
-        STORAGE_BUCKET:"flashcard-abf29.appspot.com",
-        MESSAGIN_SENDER_ID:"410065376121",
-        APP_ID:"1:410065376121:web:fb9076a87d60b898f455a8",
-      }
-    },
-    // Optionally, you can also define specific checks for 'process'
-    // to ensure compatibility with libraries that might check its type.
-    // For example, to make 'typeof process !== 'undefined'' evaluate to true:
-    'typeof process': JSON.stringify('object'),
-  },
 })
